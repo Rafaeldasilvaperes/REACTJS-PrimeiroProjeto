aea338484bd07b26c9d28aa33b1bedf772373e03
Rafael da Silva Peres <rafaeldasilvaperes@hotmail.com>
2022-09-04 13:22:29 -0300

    Usando prevStates with useState

--- src/components/ListaRender.js
+++ src/components/ListaRender.js
@@ -4,11 +4,19 @@ export const ListaRender = () => {
   const [list] = useState(["Rafael", "Renato", "Rodrigo", "Carmerinda", "Silvio", "Anna"]);
 
   // if it would have IDs
-  const [users] = useState([
+  const [users, setUsers] = useState([
     {id: 1, name: "Rafael", age: 30},
-    {id: 11231, name: "Rodrigo", age: 32},
-    {id: 134523, name: "Renato", age: 29}
+    {id: 2, name: "Rodrigo", age: 32},
+    {id: 3, name: "Renato", age: 29}
   ])
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4);
+    setUsers((prevUsers) => {
+      console.log(prevUsers)
+      return prevUsers.filter((user) => randomNumber !== user.id);
+    })
+  }
 
   return (
     <>
@@ -17,11 +25,15 @@ export const ListaRender = () => {
         <li key={i}>{item}</li>
       ))}
     </ul>
+    <hr />
     <ul>
     {users.map((user) =>(
       <li key={user.id}>{user.name} - {user.age}</li>
     ))}
+
+    <button onClick={deleteRandom}>Deletar Random User</button>
   </ul>
+  <hr />
   </>
   )
 }
