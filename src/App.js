f1d3456c12f127a7eabe5e6049ea8e71d3863beb
Rafael da Silva Peres <rafaeldasilvaperes@hotmail.com>
2022-09-06 13:31:39 -0300

    Introdução as props. Uso simples

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@
 // import CityImg from "./assets/city.jpg";
 
 // Componentes
-import { ManageData } from "./components/ManageData";
-import { ListaRender } from "./components/ListaRender";
-import { ConditionalRender } from "./components/ConditionalRender";
-
+// import { ManageData } from "./components/ManageData";
+// import { ListaRender } from "./components/ListaRender";
+// import { ConditionalRender } from "./components/ConditionalRender";
+import {ShowUserName} from "./components/ShowUserName";
 
 // Styles - CSS (global)
 import './App.css';
+import { useState } from "react";
+
 
 export function App() {
+  const name = "Joaquim de forma Dinâmica";
+  const [userName] = useState("Maria com useState");
+
   return (
     <div className="App">
      <h1>Avançando em React</h1>
@@ -22,9 +27,12 @@ export function App() {
      <div>
         {/* <img src={CityImg} alt="Cidade" /> */}
      </div>
-     <ManageData />
-     <ListaRender />
-     <ConditionalRender />
+     {/* <ManageData />
+     <ListaRender /> */}
+     {/* <ConditionalRender /> */}
+     <ShowUserName nome="Rafael direto no comp." />
+     <ShowUserName nome={name} />
+     <ShowUserName nome={userName} />
     </div>
   );
 }
---
+++ src/components/ShowUserName.js
@@ -0,0 +1,9 @@
+export const ShowUserName = (props) => {
+  return (
+    <>
+      <div>
+        <h2>O nome do usuário é: {props.nome}</h2>
+      </div>
+    </>
+  )
+}
